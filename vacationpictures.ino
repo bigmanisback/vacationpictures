ab670468f29d70bea9014b2297aceefaf2a76a75
Big Man Is Back <bigmanisback@users.noreply.github.com>
2016-02-22 09:40:05 +0200

    Print commas between each 10 bit print

--- vacationpictures.ino
+++ vacationpictures.ino
@@ -72,11 +72,17 @@ void printData() {
   Serial.print(millis());
   Serial.print(",");
   Serial.print(analogRead(1));
+  Serial.print(",");
   Serial.print(analogRead(5));
+  Serial.print(",");
   Serial.print(analogRead(0));
+  Serial.print(",");
   Serial.print(analogRead(2));
+  Serial.print(",");
   Serial.print(analogRead(3));
+  Serial.print(",");
   Serial.print(analogRead(4));
+  Serial.print(",");
   Serial.print(pressure());
   Serial.print(",");
   Serial.print(temp());
